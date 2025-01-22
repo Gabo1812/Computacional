be45336cc93a021bf8e0a145a28edc8b3e3b69b5
Gabo1812 <jose.alvarezcastrillo@ucr.ac.cr>
2025-01-21 23:47:37 -0600

    actua

--- Tarea_01/generar_random.sh
+++ Tarea_01/generar_random.sh
@@ -1,15 +1,12 @@
 #!/bin/bash
-# Asigno la cantidad de numeros aleatorios N a utilizar y la cantida de archivo i que quiero utilizar
-N=$1
-i=$1
-# Ahora vamos a generar 5 archivos ( desde i=0 a i=4) usando un  ciclo for
-for j in {0..i}
+# Asigno la cantidad de numeros aleatorios N a utilizar y la cantidad de archivos i que quiero utilizar
+N=$1  #Primer argumento de la func.
+i=$2  #Segundo argumento de la func.
+# Ahora vamos a generar 5 archivos ( desde i=0 hasta algún valor i) usando un  ciclo for
+for j in $(seq 0 $i) # Genera un rango de 0 hasta i
 do 	
-	# Ejecutar random.sh y filtrar solo líneas con números
-	random_numbers=$(./random.sh | grep -E "^[0-9]+$")
-
-	# Extraer los primeros N números
-	selected_numbers=$(echo "$random_numbers" | head -n "$N")
+	# Ejecuta random.sh y filtra solo líneas con números y extrae los primeros N valores 
+	selected_numbers=$(./random.sh | grep -E "^[0-9]+$"| head -n "$N")
 
 	# Generar el nombre del archivo 
 	output_file="rand_${j}_${N}.dat"

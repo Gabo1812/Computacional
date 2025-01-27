63885c4d1967989717eefc8c20d8d169dbb2ddf5
Gabo1812 <jose.alvarezcastrillo@ucr.ac.cr>
2025-01-27 14:50:01 -0600

    hola

--- Lab_07.3/main.cpp
+++ Lab_07.3/main.cpp
@@ -3,10 +3,10 @@
 
 int main() {
     // Crear un número complejo
-    Complex c1(3.0, 4.0);
+    Complex c(3.0, 4.0);
 
     // Mostrar la norma del número complejo c1
-    std::cout << "La norma de c1 es: " << c1.norm() << std::endl;
+    std::cout << "La norma de c1 es: " << c.norm() << std::endl;
 
     return 0;
 }
